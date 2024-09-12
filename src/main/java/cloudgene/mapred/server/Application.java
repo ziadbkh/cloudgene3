d350c8125c6e58247f3ce96947c2badab59d3686
Lukas Forer <lukas.forer@i-med.ac.at>
2024-09-12 14:14:59 +0200

    Prepare release 3.0.2

--- src/main/java/cloudgene/mapred/server/Application.java
+++ src/main/java/cloudgene/mapred/server/Application.java
@@ -29,7 +29,7 @@ import io.micronaut.context.annotation.Context;
 @Context
 public class Application {
 
-	public static final String VERSION = "3.0.1";
+	public static final String VERSION = "3.0.2";
 
 	private Database database;
 
